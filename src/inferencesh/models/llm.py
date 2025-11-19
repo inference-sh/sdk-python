d7740f179a08f4e34ad4f2e7778c7f312f21b32a
okaris <omerkarisman@gmail.com>
2025-11-19 11:25:45 +0000

    llm image encoding

--- src/inferencesh/models/llm.py
+++ src/inferencesh/models/llm.py
@@ -222,7 +222,18 @@ def timing_context():
 def image_to_base64_data_uri(file_path):
     with open(file_path, "rb") as img_file:
         base64_data = base64.b64encode(img_file.read()).decode('utf-8')
-        return f"data:image/png;base64,{base64_data}"
+        file_extension = file_path.split(".")[-1]
+        content_type = "png" 
+        if file_extension == "png":
+            content_type = "png"
+        elif file_extension == "jpg":
+            content_type = "jpg"
+        elif file_extension == "jpeg":
+            content_type = "jpeg"
+        elif file_extension == "gif":
+            content_type = "gif"
+
+        return f"data:image/{content_type};base64,{base64_data}"
 
 def build_messages(
     input_data: LLMInput,
